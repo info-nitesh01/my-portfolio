ccf08fee95b4056a0e66ceb4550db30328226b4c
nitesh-singh01 <nitesh.singh@iviewlabs.net>
2024-05-25 15:46:02 +0530

    design change

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
-// import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
