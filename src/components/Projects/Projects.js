fe0259fa5b9ca565f6f3b00a6a042d896c0d104e
nitesh-singh01 <nitesh.singh@iviewlabs.net>
2024-06-03 22:10:01 +0530

    - projects added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,9 @@ import { Col, Container, Row } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import golobe from "../../Assets/Projects/golobe.png";
 import nyus from "../../Assets/Projects/nyus.png";
+import jobportal from "../../Assets/Projects/job-portal.png";
+import sportieEbikes from "../../Assets/Projects/sportie-ebikes.png";
+
 
 function Projects() {
   return (
@@ -33,6 +36,28 @@ function Projects() {
               description="Pure HTML and CSS Template for Hotel management. Introduce the restaurant’s history, mission, and values. Highlight any unique selling points (USPs), such as farm-to-table ingredients, family recipes, or a cozy atmosphere.."
               ghLink="https://github.com/info-nitesh01/NYUS-Restaurant-Project"
               demoLink="https://nyus-food.netlify.app/#"
+            />
+          </Col>
+        </Row>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={sportieEbikes}
+              isBlog={false}
+              title="Sportie E-Bikes"
+              description="React Template for E-bike Ecommerce template with animated logo and components with JSON data and filter page."
+              ghLink="https://github.com/info-nitesh01/sportie-ebikes"
+              demoLink="https://sportie-ebikes.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={nyus}
+              isBlog={false}
+              title="Job-Portal"
+              description="Next JS Template with typeScript for Job search and upload. Added different login category using data from JSON Server, with user authentication and authorization."
+              ghLink="https://github.com/info-nitesh01/NYUS-Restaurant-Project"
+              demoLink="https://job-portal-example.netlify.app/"
             />
           </Col>
         </Row>
