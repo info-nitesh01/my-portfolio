8b6f43f47f8ab397362de6a51073d95e2b46dac3
nitesh-singh01 <nitesh.singh@iviewlabs.net>
2024-06-04 12:06:09 +0530

    job profile updated

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -8,7 +8,6 @@ function Type() {
         strings: [
           "Web Developer",
           "Front End Developer",
-          "React JS Developer",
         ],
         autoStart: true,
         loop: true,
