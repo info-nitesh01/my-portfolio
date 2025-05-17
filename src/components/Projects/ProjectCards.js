f83dd741ffb4c79bc78b6ef922b5a823afa36f70
info-nitesh01 <itsnitesh01@gmail.com>
2025-05-17 13:01:04 +0530

    added !==

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -13,7 +13,7 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        {(props.ghLink != "NA")?
+        {(props.ghLink !== "NA")?
           <Button variant="primary" href={props.ghLink} target="_blank">
             <BsGithub /> &nbsp;
             {props.isBlog ? "Blog" : "GitHub"}
