564b3d38aed8d32fabd57d95005a400659be3017
nitesh <itsnitesh01@gmail.com>
2026-08-02 20:07:34 +0530

    - changed banner image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.png";
+import homeLogo from "../../Assets/3d-boy-asset.png";
 import Home2 from "./Home2";
 import Type from "./Type";
 
@@ -10,10 +10,7 @@ function Home() {
       <Container fluid className="home-section" id="home">
         <Container className="home-content">
           <Row>
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img src={homeLogo} alt="home pic" className="img-fluid" style={{ maxHeight: "450px" }} />
-            </Col>
-            <Col md={7} className="home-header">
+            <Col md={6} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
@@ -22,13 +19,16 @@ function Home() {
               </h1>
 
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> NITESH SINGH</strong>
+                I'm
+                <strong className="main-name"> Nitesh Singh</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
                 <Type />
               </div>
+            </Col>
+            <Col md={6} className="overflow-hidden" style={{ paddingBottom: 20 }}>
+              <img src={homeLogo} alt="home pic" className="img-fluid banner-image" />
             </Col>
           </Row>
         </Container>
