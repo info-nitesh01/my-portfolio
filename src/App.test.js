fcf4ef50edbc2588753119c75ee04c246a36ece5
nitesh <itsnitesh01@gmail.com>
2026-08-02 20:54:47 +0530

    - removed boilerplate codes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("app renders without crashing", () => {
+  const div = document.createElement("div");
+  render(<App />, div);
+  unmountComponentAtNode(div);
 });
